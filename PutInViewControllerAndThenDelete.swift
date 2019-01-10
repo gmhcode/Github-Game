eb253048f943acdbe4f2a989818f806e8ad73d52
Bailey McDaris <43893086+baileylynncodes@users.noreply.github.com>
2019-01-10 15:56:20 -0700

    Revert "adds details to storyboard and provides alert code"

--- PutInViewControllerAndThenDelete.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PutInViewControllerAndThenDelete.swift
-//  GitHub Game
-//
-//  Created by Karissa McDaris on 1/10/19.
-//  Copyright © 2019 Greg Hughes. All rights reserved.
-//
-
-import Foundation

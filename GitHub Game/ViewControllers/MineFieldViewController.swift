9d6fb6d99be83d8a0997875bf5846278724c7de3
Greg Hughes <greghughes988@gmail.com>
2019-01-10 14:16:01 -0700

    made the collection view

---
+++ GitHub Game/ViewControllers/MineFieldViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MineFieldViewController.swift
+//  GitHub Game
+//
+//  Created by Greg Hughes on 1/10/19.
+//  Copyright © 2019 Greg Hughes. All rights reserved.
+//
+
+import UIKit
+
+class MineFieldViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 48
+        
+    }
+    
+    
+    
+     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)
+        
+        
+        // Configure the cell
+        
+        return cell
+    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        <#code#>
+//    }
+
+}
